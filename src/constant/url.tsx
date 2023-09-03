7a317f2ab2a62a6e33fbcd64a7d4c94c2da33bbb
momen <20211270@science.helwan.edu.eg>
2023-09-03 07:19:57 +0200

    update BASE_URL

--- src/constant/url.tsx
+++ src/constant/url.tsx
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://www.omdbapi.com/?apikey=";
+export const BASE_URL = "https://www.omdbapi.com/?apikey=";
 export const REVIEW_URL =
   'https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name%3A"Movies" AND type_of_material%3A"Review"&sort=newest&page=0';
